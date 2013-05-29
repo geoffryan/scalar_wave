9bd2f0a512a83a9d6af9cc7dd1199ccbe8963bac
Geoff Ryan <geoffrey.s.ryan@gmail.com>
2013-05-29 15:08:21 -0400

    First commit!

---
+++ scalar_wave.c
@@ -0,0 +1,18 @@
+/*
+ *  scalar_wave.c
+ *  
+ *
+ *  Created by Geoffrey Ryan on 13-05-29.
+ *  Copyright 2013 None. All rights reserved.
+ *
+ */
+
+#include "scalar_wave.h"
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
+
---
+++ scalar_wave.h
@@ -0,0 +1,9 @@
+/*
+ *  scalar_wave.h
+ *  
+ *
+ *  Created by Geoffrey Ryan on 13-05-29.
+ *  Copyright 2013 None. All rights reserved.
+ *
+ */
+
